ba428a4d4da363b6a543e9ca51cc42775e3f4cb2
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-12-24 14:58:30 +0100

    project init

---
+++ src/main/java/com/reviews/rhmanagement/RhManagementApplication.java
@@ -0,0 +1,13 @@
+package com.reviews.rhmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reviews/rhmanagement/RhManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviews.rhmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
